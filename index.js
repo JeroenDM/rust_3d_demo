b659e2b4485b4af65a2f1c9b36df8d039195c0f7
jeroendm <jeroendemaeyer@live.be>
2021-12-18 15:13:03 +0100

    continue with tutorial part 1

---
+++ index.js
@@ -0,0 +1,44 @@
+const rust = import('./pkg/rust_3d_demo');
+const canvas = document.getElementById('rustCanvas');
+const gl = canvas.getContext('webgl', { antialias: true });
+
+rust.then(m => {
+    if (!gl) {
+        alert('Failed to initialize WebGL');
+        return;
+    }
+
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+    const FPS_THROTTLE = 1000.0 / 30.0; // milliseconds / frames
+    var lastDrawTime = -1; // In milliseconds
+
+    function render() {
+        window.requestAnimationFrame(render);
+        const currTime = Date.now();
+
+        // Update canvas dimensions when user resizes window.
+        if (window.innerHeight !== canvas.height || window.innerWidth !== canvas.width) {
+            canvas.height = window.innerHeight;
+            canvas.clientHeight = window.innerHeight;
+            canvas.style.height = window.innerHeight;
+
+            canvas.width = window.innerWidth;
+            canvas.clientWidth = window.innerWidth;
+            canvas.style.width = window.innerWidth;
+
+            // console.log(window.innerWidth, window.innerHeight);
+
+            gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+        }
+
+        if (currTime >= lastDrawTime + FPS_THROTTLE) {
+            lastDrawTime = currTime;
+            // Rust update call.
+            // Rust render call.
+        }
+    }
+
+    render();
+});
---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s : &str);
+}
+
+#[wasm_bindgen]
+pub fn say_hello_from_rust() {
+    log("Howdy!...from Rust and more.");
+
+}
+
+#[wasm_bindgen]
+pub struct MyClient {
+
+}
+
+#[wasm_bindgen]
+impl MyClient {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
+        Ok(())
+    }
+
+    pub fn render(&self) {
+
+    }
+}
